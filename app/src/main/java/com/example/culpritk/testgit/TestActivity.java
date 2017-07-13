3b40899fb3354414e7fd08f9eda9c46a15390fb0
jbkim76 <culpritk@naver.com>
2017-07-13 16:29:15 +0900

    test_layout 뒤로가기 버튼 추가

--- app/src/main/java/com/example/culpritk/testgit/TestActivity.java
+++ app/src/main/java/com/example/culpritk/testgit/TestActivity.java
@@ -1,7 +1,10 @@
 package com.example.culpritk.testgit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_layout);
+
+        Button btnBack = (Button)findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "뒤돌아가기", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
     }
 }
